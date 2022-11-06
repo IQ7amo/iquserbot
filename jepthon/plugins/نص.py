e71f0a3ae3aee9d028aee31d2f5a782f0c11f1d8
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-11-07 02:17:33 +0300

    Update نص.py

--- jepthon/plugins/نص.py
+++ jepthon/plugins/نص.py
@@ -1,4 +1,4 @@
-"""QuotLy: Avaible commands: .تحويل
+"""QuotLy: Avaible commands: .گۆڕین
 """
 import datetime
 import asyncio
@@ -7,33 +7,33 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from jepthon.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern="تحويل نص ?(.*)"))
+@borg.on(admin_cmd(pattern="گۆڕینی دەق ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("᯽︙ يـجب. الرد علـى رسـالة الـمستخدم )")
+       await event.edit("᯽︙ پێویستە. وەڵامدانەوەی نامەی بەکارهێنەر )")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("᯽︙ يـجب. الرد علـى رسـالة الـمستخدم )")
+       await event.edit("᯽︙ پێویستە. وەڵامدانەوەی نامەی بەکارهێنەر )")
        return
     chat = "@QuotLyBot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("᯽︙ يـجب. الرد علـى رسـالة الـمستخدم )")
+       await event.edit("᯽︙ پێویستە. وەڵامدانەوەی نامەی بەکارهێنەر )")
        return
-    await event.edit("᯽︙ جار تحويل النص الى ملصق")
+    await event.edit("᯽︙ گۆڕینی دەق بۆ ستیکەر ..🕷️🖤")
     async with event.client.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
               await event.client.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock me (@QuotLyBot) u Nigga```")
+              await event.reply("```بلۆکم لابەرە گەمژە (@QuotLyBot)```")
               return
           if response.text.startswith("Hi!"):
-             await event.edit("᯽︙ يجـب الغاء خصـوصية التوجيـه اولا")
+             await event.edit("᯽︙ تایبەتمەندی ڕێنماییەکە دەبێت یەکەم جار هەڵوەشێتەوە 🕷️")
           else: 
              await event.delete()
              await event.client.send_message(event.chat_id, response.message)
