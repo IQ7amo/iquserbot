f751c543d0c0ba652b225aa855930eec4896e183
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-11-12 11:48:58 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChatBannedRights
 
 class Config(object):
     LOGGER = True
-
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/3851323764f1629e16ce8.jpg")
     A_PIC = os.environ.get("A_PIC", None)
     ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
     SC_TEXT = os.environ.get("SCPIC_TEXT", None)
