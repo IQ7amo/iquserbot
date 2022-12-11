501e5360ca1cb0322c516d51edcbc96c1689b81b
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-12-12 02:03:30 +0300

    Update session.py

--- jepthon/core/session.py
+++ jepthon/core/session.py
@@ -14,7 +14,7 @@ loop = None
 if Config.STRING_SESSION:
     session = StringSession(str(Config.STRING_SESSION))
 else:
-    session = "IQ bot"
+    session = "jepthon"
 
 try:
     jepiq = CatUserBotClient(
